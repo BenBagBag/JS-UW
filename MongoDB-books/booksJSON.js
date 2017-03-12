74a6a67c667b91bac3ba6b14dc91f3abe9f9e726
BenBagBag <IsaacC@allenai.org>
2017-03-11 21:41:06 -0800

    collate HW files in one place

---
+++ MongoDB-books/booksJSON.js
@@ -0,0 +1,62 @@
+db.books.insert([
+  {
+    title: 'Wool',
+    author: 'Hugh Howey',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    title: 'Dune',
+    author: 'Frank Herbert',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    title: 'The Fall of the Ottomans',
+    author: 'Eugene Rogan',
+    genre: 'History',
+    read: false
+  },
+  {
+    title: 'Empire Falls',
+    author: 'Richard Russo',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    title: 'Shift',
+    author: 'Hugh Howey',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    title: 'Truman',
+    author: 'David McCullough',
+    genre: 'History',
+    read: false
+  },
+  {
+    title: 'The Amazing Adventures of Kavalier and Klay',
+    author: 'Michael Chabon',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    title: 'Ulysses',
+    author: 'James Joyce',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    title: 'The Hobbit',
+    author: 'J.R. Tolkien',
+    genre: 'Fantasy',
+    read: false
+  }
+]);
